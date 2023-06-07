bcadad610cb7f4373e29c73a2be67aa693e7cae8
naloyd <naloyd@iupui.edu>
2023-06-07 18:04:26 -0400

    After image seems to be working

--- flashbang.py
+++ flashbang.py
@@ -5,54 +5,60 @@ from pygame.locals import *
 
 class Flashbang:
     def __init__(self, screen, width, height):
-        #self.whiteSquare = pygame.Rect((0, 0, width, height))
-        self.on = False
-        self.timer = 0
+        self.screen = screen
         self.width = width
         self.height = height
-        self.screen = screen
-
-    def trigger(self, stunTime=500):
-        self.on = True
-        self.timer = stunTime
-
-        # Get a freeze frame of the current screen
-        #self.freezeFrame = pygame.image.save(self.screen, "freezeFrame.png")
+        self.whiteoutSurface = pygame.Surface((width, height))
+        self.whiteoutSurface.fill((255, 255, 255))
+        self.whiteoutSurface.set_alpha(0)  # Initial alpha value of 0
+        self.on = False
+        self.timer = 0
         self.freezeFrame = pygame.image.load("freezeFrame.png")
 
+    def trigger(self, stunTime=200):
+        self.on = True
+        self.timer = stunTime
+        self.stunTime = stunTime
+        print("triggered")
+
     def update(self):
-        # Once the flashbang is triggered, it will stay on for the specified time
         if self.on:
             self.timer -= 1
             if self.timer <= 0:
                 self.on = False
                 self.timer = 0
-            print(self.timer)
 
-    def brightnessFunction(self, x):
-        # This function returns the brightness of the flashbang at a given distance x
-        if 0 <= x <= 20: return 1 - np.exp(-10 * x)
-        elif 20 <= x <= 100: return 1
-        elif 100 <= x <= 400: return 1 - 0.005 * (x - 100)
-        else: return 0
-    
-    def afterimageBrightnessFunction(self, x):
-        if x < 100: return 0
-        elif 100 <= x < 200: return (x - 100) / 100 * 0.33
-        elif 200 <= x < 300: return 0.33
-        elif 300 <= x <= 400: return 0.33 - (x - 300) / 100 * 0.33
-        else: return 0
-        
+    def brightnessFunction(self):
+        animationPercentage = 1 - self.timer / self.stunTime
+        if animationPercentage <= 0.01: return animationPercentage / 0.01  # Rapidly ramp up from 0 to 1
+        elif 0.01 < animationPercentage <= 0.2: return 1  # Hold at 1 for 20% of the animation
+        else: return 1 - (animationPercentage - 0.2) / 0.4  # Slowly fade from 1 to 0
+
+
+    def afterimageBrightnessFunction(self):
+        animationPercentage = self.timer / self.stunTime
+        if animationPercentage <= 0.1: return 0  # Hold at 0 for the first 10% of the animation
+        elif 0.1 < animationPercentage <= 0.4: return (animationPercentage - 0.1) / 0.3 * 0.33  # Ramp up to 33%
+        else: return 0.33 - (animationPercentage - 0.4) / 0.6 * 0.33  # Slowly fade from 33% to 0
+
     def draw(self):
         if self.on:
-            brightness = self.brightnessFunction(self.timer/60) * 255
-            color = (int(brightness), int(brightness), int(brightness))
-            pygame.draw.rect(self.screen, color, self.screen.get_rect())
-            
-            # draw the flashbang freeze frame with an alpha value
+            # Calculate brightness for both flashbang and afterimage
+            flashbangBrightness = self.brightnessFunction()
+            afterimageBrightness = self.afterimageBrightnessFunction()
 
-            brightnessFreezeFrame = self.afterimageBrightnessFunction(self.timer) * 255
-            self.freezeFrame.set_alpha(brightnessFreezeFrame)
+            # Determine the alpha values based on the brightness values
+            flashbangAlpha = int(flashbangBrightness * 255)
+            afterimageAlpha = int(afterimageBrightness * 255)
+
+            # Set alpha values for the whiteout surface and the afterimage surface
+            self.whiteoutSurface.set_alpha(flashbangAlpha)
+            self.freezeFrame.set_alpha(afterimageAlpha)
+
+            # Draw the whiteout surface to cover the entire screen
+            self.screen.blit(self.whiteoutSurface, (0, 0))
+
+            # Draw the freeze frame afterimage on top of the whiteout surface
             self.screen.blit(self.freezeFrame, (0, 0))
 
 def main():
@@ -64,18 +70,16 @@ def main():
     clock = pygame.time.Clock()
     fps = 60
     flashbang = Flashbang(screen, width, height)
-    
-    triggerTest = 100
+    timer = 200
+
 
     while True:
-        # Trigger the flashbang
-        triggerTest = max(triggerTest-1, 0)
-        if triggerTest <= 0:
+        timer -= 1
+        if timer <= 0:
             flashbang.trigger()
-            triggerTest = 1000
-            
-        # Draw the flashbang to the screen
+            timer = 1000
         screen.fill((0, 0, 0))
+
         flashbang.draw()
         flashbang.update()
 
