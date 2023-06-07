0e496baca3f088428546abfd0a4cb968f5d3db09
naloyd <naloyd@iupui.edu>
2023-06-07 15:42:28 -0400

    added a new brigneess function about to test it

--- flashbang.py
+++ flashbang.py
@@ -1,30 +1,34 @@
-import pygame, sys, os, math
+import pygame, sys, os, math, np
 from pygame.locals import *
 
-
-
-
-
 class Flashbang():
-    
-
-    def __init__(self, screen, width, height):
+    def __init__(self, width, height):
         self.whiteSquare = pygame.Rect((0, 0, width, height))
         self.on = False
         self.timer = 0
 
-    def trigger(self):
+    def trigger(self, stunTime = 100):
         self.on = True
+        self.timer = stunTime
+
+        # get a freeze frame of the current screen
+        self.freezeFrame = pygame.Surface((self.width, self.height))
 
     def update(self):
         # once the flashbang is triggered, it will stay on for 1 second
         if self.on:
-            self.timer += 1
-            if self.timer >= 60:
+            self.timer -= 1
+            if self.timer <= 0:
                 self.on = False
                 self.timer = 0
             print(self.timer)
 
+    def brightnessFunction(x):
+        # this function will return the brightness of the flashbang at a given distance, and time
+        if 0 <= x <= 5: return 1 - np.exp(-10 * x)
+        elif 5 <= x <= 100: return 1
+        elif 100 <= x <= 400: return 1 - 0.005 * (x - 100)
+        else: return 0
 
 def main():
     pygame.init()
@@ -36,11 +40,6 @@ def main():
     fps = 60
     flashbang = Flashbang(screen, width, height)
     flashbang.trigger()
-
-    
-
-
-
 
     while True:
         # draw the flashbang to the screen
