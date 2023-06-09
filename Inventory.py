e1c25b54eee2ceec2a1e8662858ee525687eb91a
naloyd <naloyd@iupui.edu>
2023-06-09 17:05:37 -0400

    Made inventory the same method structure the new

--- Inventory.py
+++ Inventory.py
@@ -18,6 +18,49 @@ class Inventory:
         self.visible = True
         self.interactDelay = 0
 
+    def print(self):
+        for item in self.inventory:
+            print(f"{item.name} {item.count}")
+    
+
+    def addItem(self, Item):
+        desiredName = Item.name
+     
+        # remove the item from the inventory array
+        for item in self.inventory:
+            if item.name == desiredName:
+                Item.count += item.count
+                self.inventory.remove(item)
+                print('removed item')
+        
+        # this is done so items are added to the front of the array
+        self.inventory = [Item] + self.inventory
+        self.limitInventoryToMax(desiredName)
+
+    def limitInventoryToMax(self, name):
+        if name not in self.maxInventory(): return
+        for item in self.inventory:
+            if item.name == name:
+                item.count = min(item.count, self.maxInventory()[name])    
+
+    def removeCurrentItem(self):
+        self.inventory[0].count -= 1
+        if self.inventory[0].count <= 0:
+            self.inventory.pop(0)
+
+    def currentItem(self):
+        return self.inventory[0]
+
+    def currentItemCount(self):
+        return self.inventory[0].count
+
+    def selectLeft(self):
+        self.inventory = [self.inventory[-1]] + self.inventory[0:-1]
+        
+    def selectRight(self):
+        self.inventory = self.inventory[1:] + self.inventory[:1]
+
+
     def maxInventory(self):
         return {'smg': 100, 'pistol': 69, 'sniper':100, 'gun':20}  
     
@@ -41,37 +84,6 @@ class Inventory:
         keeps = ['fiberWire', 'camera']
         return keep in keeps
 
-    def addItem(self, Item):
-        desiredName = Item.name
-     
-        # remove the item from the inventory array
-        for item in self.inventory:
-            if item.name == desiredName:
-                Item.count += item.count
-                self.inventory.remove(item)
-                print('removed item')
-        
-        # this is done so items are added to the front of the array
-        self.inventory = [Item] + self.inventory
-        self.limitInventoryToMax(desiredName)
-
-    def limitInventoryToMax(self, name):
-        if name not in self.maxInventory(): return
-        for item in self.inventory:
-            if item.name == name:
-                item.count = min(item.count, self.maxInventory()[name])
-
-    def currentItemCount(self):
-        return self.inventory[0].count
-    
-    def currentItem(self):
-        return self.inventory[0]
-
-    def removeCurrentItem(self):
-        self.inventory[0].count -= 1
-        if self.inventory[0].count <= 0:
-            self.inventory.pop(0)
-
     def shoot(self):
         # if not a gun don't shoot
         if not self.isCurrentItemGun(): return
@@ -80,22 +92,18 @@ class Inventory:
         if gun.count <= 0: return
         gun.count -= 1
 
-    def print(self):
-        for item in self.inventory:
-            print(f"{item.name} {item.count}")
-
-    def selectLeft(self):
-        self.inventory = [self.inventory[-1]] + self.inventory[0:-1]
-        
-    def selectRight(self):
-        self.inventory = self.inventory[1:] + self.inventory[:1]
-
     def drawCount(self):
         #if not self.isCurrentItemGun(): return
         gun = self.currentItem()
         font = pygame.font.SysFont(None, 30)
         ammo = font.render(str(gun.count), True, (255,255,255))
         self.grandparent.screen.blit(ammo, (0,0))
+
+    def drawCarousel(self):
+        pygame.draw.rect(self.grandparent.screen, (255,255,255), (0, self.grandparent.height - 100, self.grandparent.width, 100))
+        for i in range(len(self.inventory)):
+            item = self.inventory[i]
+            self.grandparent.screen.blit(pygame.image.load(os.path.join('sprites','items', item.name + '.png')), (i*100, self.grandparent.height - 75))
 
     def update(self):
         self.interactDelay = max(0, self.interactDelay - 1)
@@ -118,12 +126,4 @@ class Inventory:
         if keysPressed[K_RIGHT] and self.interactDelay == 0:
             self.selectRight()
             self.interactDelay = 10
-
-    def drawCarousel(self):
-        pygame.draw.rect(self.grandparent.screen, (255,255,255), (0, self.grandparent.height - 100, self.grandparent.width, 100))
-        for i in range(len(self.inventory)):
-            item = self.inventory[i]
-            self.grandparent.screen.blit(pygame.image.load(os.path.join('sprites','items', item.name + '.png')), (i*100, self.grandparent.height - 75))
-
-
-
+            
